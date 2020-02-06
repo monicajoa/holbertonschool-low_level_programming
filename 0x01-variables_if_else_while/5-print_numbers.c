f894250cb16131804c52b5d7d041e6d96f3cc938
monicajoa <mnortiz.ortiz@gmail.com>
2020-02-06 23:11:09 +0000

    0x01. C - Variables, if, else, while, 5. Number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	n = 0;
+	while (n <= 9)
+	{
+		printf("%d", n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
