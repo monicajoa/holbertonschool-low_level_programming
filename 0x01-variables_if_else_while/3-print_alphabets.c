190198fa4fbf5f03416f6ed0e9815dc1965fed90
monicajoa <mnortiz.ortiz@gmail.com>
2020-02-06 21:54:31 +0000

    0x01. C - Variables, if, else, while, 3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lc;
+	char uc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+		putchar(lc);
+	}
+	for (uc = 'A'; uc <= 'Z'; uc++)
+	{
+		putchar(uc);
+	}
+	putchar('\n');
+	return (0);
+}
