917f55e887635eb0d72e2e2d6497ad442db57d13
monicajoa <mnortiz.ortiz@gmail.com>
2020-02-06 23:39:32 +0000

    0x01. C - Variables, if, else, while, 7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lc;
+
+	for (lc = 'z'; lc >= 'a'; lc--)
+	{
+		putchar(lc);
+	}
+	putchar('\n');
+	return (0);
+}
