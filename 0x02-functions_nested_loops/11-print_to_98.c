b240f50153fae2221a2c718748136504cc1a6c01
monicajoa <mnortiz.ortiz@gmail.com>
2020-02-12 04:59:42 +0000

    0x02. C - Functions, nested loops, 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -18,7 +18,7 @@ void print_to_98(int n)
 			if (cont == 98)
 			printf("%d", cont);
 			else
-			printf("%d ,", cont);
+			printf("%d, ", cont);
 		}
 		putchar('\n');
 	}
@@ -29,7 +29,7 @@ void print_to_98(int n)
 			if (cont == 98)
 			printf("%d", cont);
 			else
-			printf("%d ,", cont);
+			printf("%d, ", cont);
 		}
 		putchar('\n');
 	}
