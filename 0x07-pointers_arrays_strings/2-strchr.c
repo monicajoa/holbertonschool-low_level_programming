5b888063503e251afe173c03c08a55c759514107
monicajoa <mnortiz.ortiz@gmail.com>
2020-02-24 19:48:52 +0000

    0x07. C - Even more pointers, arrays and strings, 2. strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ *_strchr - function that locates a character in a string.
+ *@s:the pointer with the locatation of the string
+ *@c:the character to find in the string
+ *
+ * Return: Always 0 (Success)
+ */
+char *_strchr(char *s, char c)
+{
+	int index;
+
+	index = 0;
+	while (s[index] != '\0')
+	{
+		if (s[index] == c)
+		{
+			return (s + index);
+		}
+		index++;
+	}
+	return (s + index);
+}
