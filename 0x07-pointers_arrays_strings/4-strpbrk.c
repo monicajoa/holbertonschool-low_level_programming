f8564096bd8cfb75fc217d12b19b4d706d84cada
monicajoa <mnortiz.ortiz@gmail.com>
2020-02-24 23:28:06 +0000

    0x07. C - Even more pointers, arrays and strings, 4. strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+/**
+ *_strpbrk - function that searches a string for any of a set of bytes
+ *@s:the string to check
+ *@accept:letters to accept and count
+ *
+ * Return: Always 0 (Success)
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int index_1;
+	int index_2;
+	int n;
+
+	n = 0;
+	while (s[n] != '\0')
+	{
+		n++;
+	}
+	index_1 = 0;
+	while (index_1 <= n)
+	{
+		index_2 = 0;
+		while (accept[index_2] != '\0')
+		{
+			if (s[index_1] == accept[index_2])
+			{
+				return (s + index_1);
+			}
+			else if (s[index_1] == '\0')
+			{
+				s = '\0';
+			}
+		index_2++;
+		}
+	index_1++;
+	}
+	return (s + index_1);
+}
