804cb169383383f18a03bd41a5ecf65b010d51a4
monicajoa <mnortiz.ortiz@gmail.com>
2020-02-26 04:58:56 +0000

    0x07. C - Even more pointers, arrays and strings, 8. Double pointer, double fun

--- 0x07-pointers_arrays_strings/9-set_string.c
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -2,11 +2,11 @@
 /**
  * set_string - sets the value of a pointer to a char.
  * @s:the pointer that have the addres with the string
- * @to ponter where tha value where change
+ * @to: pointer where tha value where change
  *
  * Return: Always 0 (Success)
  */
 void set_string(char **s, char *to)
 {
-	*s = to; 
+	*s = to;
 }
