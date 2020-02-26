a4b8de3ec257c11fce58d19d0493e1dad017bab4
monicajoa <mnortiz.ortiz@gmail.com>
2020-02-26 05:18:16 +0000

    0x07. C - Even more pointers, arrays and strings, 7. The line of life is a ragged diagonal between duty and desire

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * _print_diagsums - prints the sum of the two diagonals of a matrix.
+ * @a: pointer with the address of the matrix
+ * @size: int that represents the size of the matrix
+ *
+ * Return: Always 0 (Success)
+ */
+void print_diagsums(int *a, int size)
+{
+	int index1;
+	int index2;
+	int cont_dig1 = 0;
+	int cont_dig2 = 0;
+	int icrmt1 = 0;
+	int icrmt2 = 0;
+
+	index1 = 0;
+	while (index1 < size)
+	{
+		icrmt1 = ((size + 1) * index1);
+		cont_dig1 = cont_dig1 + a[icrmt1];
+		index1++;
+	}
+	index2 = 1;
+	while (index2 <= size)
+	{
+		icrmt2 = ((size - 1) * index2);
+		cont_dig2 = cont_dig2 + a[icrmt2];
+		index2++;
+	}
+	printf("%d, %d\n", cont_dig1, cont_dig2);
+}
