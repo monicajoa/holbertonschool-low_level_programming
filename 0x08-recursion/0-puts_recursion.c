a1d8b136cc43297888fec39aff7d3516a2cc6ae9
monicajoa <mnortiz.ortiz@gmail.com>
2020-02-26 19:18:20 +0000

    0x08. C - Recursion, 0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ *_puts_recursion - function that prints a string
+ *@s:pointer with the address of the first position of the string
+ *
+ * Return: Always 0 (Success)
+ */
+void _puts_recursion(char *s)
+{
+	int i;
+
+	i = 0;
+
+	if (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+		_puts_recursion(&s[i]);
+	}
+	else 
+	{
+		_putchar('\n');
+	}
+}
--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -1,7 +1,8 @@
 #ifndef PEER
 #define PEER
 
-
+int _putchar(char c);
+void _puts_recursion(char *s);
 
 
 #endif
