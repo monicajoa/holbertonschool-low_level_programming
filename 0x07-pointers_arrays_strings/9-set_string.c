d8b896c3bfd6d8f2441e33dd9a971352f0266ab7
monicajoa <mnortiz.ortiz@gmail.com>
2020-02-26 04:56:50 +0000

    0x07. C - Even more pointers, arrays and strings, 8. Double pointer, double fun

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * set_string - sets the value of a pointer to a char.
+ * @s:the pointer that have the addres with the string
+ * @to ponter where tha value where change
+ *
+ * Return: Always 0 (Success)
+ */
+void set_string(char **s, char *to)
+{
+	*s = to; 
+}
