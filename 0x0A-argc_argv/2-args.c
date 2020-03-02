e3ae97efc543920176208856469c5c3ddf3abe1e
monicajoa <mnortiz.ortiz@gmail.com>
2020-03-02 00:37:17 +0000

    0x0A. C - argc, argv, 2. The best argument against democracy is a five-minute conversation with the average voter

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - Program that prints all arguments it receives.
+ *@argc:the number of command line arguments and size of argv.
+ *@argv:array containing the program command line arguments.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
