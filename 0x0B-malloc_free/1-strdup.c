ee1793dd4730eabe3760bd88b59d736ede254db1
monicajoa <mnortiz.ortiz@gmail.com>
2020-03-03 20:42:44 +0000

    0x0B. C - malloc, free, 1. The woman who has no imagination has no wings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ *_strdup - function that returns a pointer to a newly allocated space in memory
+ *@str: 
+ *
+ * Return: Always 0 (Success)
+ */
+char *_strdup(char *str)
+{
+
+	int i;
+	int a;
+	char *m;
+
+	if (str == '\0')
+	{
+		return ('\0');
+	}
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	m = malloc(i * sizeof(char));
+	if (m == '\0')
+	{
+		return ('\0');
+	}
+	for (a = 0; a < i; a++)
+	{
+		m[a] = str[a];
+	}
+	return (m);
+}
