e86efd1dbd57251bcc8b4d513a13510aa068188d
monicajoa <mnortiz.ortiz@gmail.com>
2020-03-09 22:21:22 +0000

    0x0E. C - Structures, typedef, 0. Django

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,21 @@
-#ifndef PEER
-#define PEER
+#ifndef DOG
+#define DOG
 
 int _putchar(char c);
+
+/**
+ * struct dog - dog information
+ * @name: dog's name
+ * @owner: ownwe's name
+ * @age: dog's age
+ *
+ * Description: Dog Information
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
 
 #endif
