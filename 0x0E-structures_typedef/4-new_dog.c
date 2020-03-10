7700ee7de3a95bdfc5ab2e83c010f1c91d995603
monicajoa <mnortiz.ortiz@gmail.com>
2020-03-10 00:29:55 +0000

    0x0E. C - Structures, typedef, 4. A door is what a dog is perpetually on the wrong side of, add strdup function

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,39 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
+ *_strdup -  returns a pointer to a newly allocated space in memory
+ *@str: duplicate of the string
+ *
+ * Return: Always 0 (Success)
+ */
+char *_strdup(char *str)
+{
+
+	int i;
+	int a;
+	char *m;
+
+	if (str == '\0')
+	{
+		return ('\0');
+	}
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	m = malloc((i * sizeof(char)) + 1);
+	if (m == '\0')
+	{
+		return ('\0');
+	}
+	for (a = 0; a <= i; a++)
+	{
+		m[a] = str[a];
+	}
+	return (m);
+}
+/**
  *new_dog - function that creates a new dog.
  *@name:dog's name
  *@age:dog's age
@@ -15,8 +48,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	m = malloc(sizeof(dog_t));
 	if (m == '\0')
 		return ('\0');
-	m->name = name;
+	m->name = _strdup(name);
 	m->age = age;
-	m->owner = owner;
+	m->owner = _strdup(owner);
 	return (m);
 }
