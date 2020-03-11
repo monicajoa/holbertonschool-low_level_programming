e897beb627b96f899c7095e3d61cb8b09891ca3b
monicajoa <mnortiz.ortiz@gmail.com>
2020-03-11 21:39:41 +0000

    0x0F. C - Function pointers, 2. To hell with circumstances; I create opportunities

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ *int_index - function that searches for an integer
+ *@array: array of integers
+ *@size: is the number of elements in the array
+ *@cmp: pointer to the function to be used to compare values
+ *
+ * Return: Always 0 (Success)
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == '\0' || cmp == '\0')
+	{
+		return (- 1);
+	}
+	if (size <= 0)
+	{
+		return (- 1);
+	}
+	for (i = 0; i <= size; i++)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+	}
+	return (- 1);
+}
