fcfd40f27a73edf3794bcbd0848c5f0156795ffd
monicajoa <mnortiz.ortiz@gmail.com>
2020-03-23 17:11:35 +0000

    0x13. C - More singly linked lists, 0. Print list, Readme, list.h, main-0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ *print_listint - function that prints all the elements of a list
+ *@h: pointer type list_t (structure) with the head of the list
+ *
+ * Return: unsigned int with the numbers printed
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h != '\0')
+	{
+		printf("%u\n", h->n);
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
