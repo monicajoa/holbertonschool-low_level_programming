692d51332b3770bf0750f3d57b9c6a49066f75f5
monicajoa <mnortiz.ortiz@gmail.com>
2020-03-25 03:16:04 +0000

    0x13. C - More singly linked lists, 7. Get node at index, change loop

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -16,8 +16,12 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	{
 		return (NULL);
 	}
-	while (i < index)
+	while (m != NULL)
 	{
+		if (i == index)
+		{
+			return (m);
+		}
 		m = m->next;
 		i++;
 	}
