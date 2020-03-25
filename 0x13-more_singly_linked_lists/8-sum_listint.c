7490b43502ae4f2bb65d653efc308179424146b3
monicajoa <mnortiz.ortiz@gmail.com>
2020-03-25 04:14:06 +0000

    0x13. C - More singly linked lists, 8. Sum list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ *sum_listint - returns the sum of all the data (n) of a linked list
+ *@head: pointer with the first node
+ *
+ * Return: integer with the sum of all the data (n)
+ */
+int sum_listint(listint_t *head)
+{
+	listint_t *m;
+	int sum;
+
+	m = head;
+	if (head == NULL)
+	{
+		return (0);
+	}
+	while (m != NULL)
+	{
+		sum = sum + (m->n);
+		m = m->next;
+	}
+	return (sum);
+}
