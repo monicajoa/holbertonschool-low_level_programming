4f85c2b4f016ee5d0bb58473308c1b14c9cc59fe
monicajoa <mnortiz.ortiz@gmail.com>
2020-03-25 02:46:51 +0000

    0x13. C - More singly linked lists, 7. Get node at index, add condition

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -14,6 +14,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	{
 		return (NULL);
 	}
+	if (index == 0)
+	{
+		return (head);
+	}
 	while (i < index)
 	{
 		head = head->next;
