708bb05329a3fe395a443612dde4e91e3f520995
monicajoa <mnortiz.ortiz@gmail.com>
2020-03-27 04:43:24 +0000

    0x14. C - Bit manipulation, 5.101

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ *flip_bits - returns the number of bits
+ *@n: decimal number 1
+ *@m: decimal number 2
+ *
+ * Return: number of bits to get another number
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int aux;
+	unsigned long b;
+
+	aux = n ^ m;
+	b = 0;
+	while (aux != 0)
+	{
+		if ((aux & 1) == 1)
+		{
+			b++;
+		}
+		aux = aux >> 1;
+	}
+	return (b);
+}
