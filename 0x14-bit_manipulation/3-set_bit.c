04cf0870914a1b921e97cc1b82b86bd7ef07e8c1
monicajoa <mnortiz.ortiz@gmail.com>
2020-03-27 04:17:00 +0000

    0x14. C - Bit manipulation, 3.11

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ *set_bit - that sets the value of a bit to 1 at a given index
+ *@n: decimal number
+ *@index: is the index, starting from 0 of the bit you want to set
+ *
+ * Return: 1 if it worked, or -1 if an error occurred
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	int b = 1;
+
+	if (index > (sizeof(n) * 8) - 1)
+	{
+		return (-1);
+	}
+	b = b << index;
+	*n = (*n | b);
+	return (1);
+}
