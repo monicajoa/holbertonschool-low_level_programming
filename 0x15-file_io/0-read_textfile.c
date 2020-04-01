a14dd95f442e46e3d545b4484242534dabdc36bb
monicajoa <mnortiz.ortiz@gmail.com>
2020-04-01 04:01:08 +0000

    0x15. C - File I/O, 0. Tread lightly, she is near, add description return

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,7 +3,7 @@
  *read_textfile - reads a text file and prints it to the POSIX standard output
  *@filename : file to read to print
  *@letters : number of letters it should read and print
- *Return : actual number of letters it could read and print
+ *Return: actual number of letters it could read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
