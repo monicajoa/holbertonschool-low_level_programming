8610120b48da7e1ad2900599ed00770da68f4df9
monicajoa <mnortiz.ortiz@gmail.com>
2020-04-01 04:45:35 +0000

    0x15. C - File I/O, 1. Under the snow

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+ *create_file - creates a file.
+ *@filename: name of the file to create
+ *@text_content: is a NULL terminated string to write to the file
+ *Return: actual number of letters it could read and print
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int op, wt;
+	int j = 0;
+
+	if (filename == '\0')
+	{
+		return (-1);
+	}
+	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (op == -1)
+	{
+		return (-1);
+	}
+	if (text_content == '\0')
+	{
+		return (-1);
+	}
+	while (text_content[j] != '\0')
+	{
+		j++;
+	}
+	wt = write(op, text_content, j);
+	if (wt == -1)
+	{
+		return (-1);
+	}
+	close(op);
+	return (1);
+}
