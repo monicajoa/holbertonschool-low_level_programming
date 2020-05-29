77da1928a75275ecdf4863d3ca4acf47d471a219
monicajoa <mnortiz.ortiz@gmail.com>
2020-05-29 18:31:09 -0500

    0x1A. C - Hash tables, task 2, key -> index, main, file, header

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - Function that gives you the index of a key.
+ * @key: is the key
+ * @size: the size of the array of the hash table
+ *
+ * Return: index at which the key/value should be stored
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index_hash;
+	unsigned long int index_table;
+
+	index_hash = hash_djb2(key);
+	index_table = index_hash % size;
+	return (index_table);
+}
