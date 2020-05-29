81ae1dcd8ce5490572cfe1e4cff8a7fa2d7caa22
monicajoa <mnortiz.ortiz@gmail.com>
2020-05-29 17:02:09 -0500

    0x1A. C - Hash tables, task 0. >>> ht = {}, add file,code fix, initialize using calloc

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,13 +14,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 	{
 		return (NULL);
 	}
-	new_table = malloc(sizeof(hash_table_t));
+	new_table = calloc(1, sizeof(hash_table_t));
 	if (new_table == NULL)
 	{
 		return (NULL);
 	}
 	new_table->size = size;
-	new_table->array = malloc(size * sizeof(hash_node_t));
+	new_table->array = calloc(size, sizeof(hash_node_t));
 	if (new_table->array == NULL)
 	{
 		free(new_table);
