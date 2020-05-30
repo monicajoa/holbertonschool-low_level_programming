3f5eb9dda7d78d74a8ed56380d21e2178f4591ad
monicajoa <mnortiz.ortiz@gmail.com>
2020-05-29 20:48:42 -0500

    0x1A. C - Hash tables, task 3, >>> ht['betty'] = 'holberton' file, check if key exist of the current linked list to update

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,6 +1,30 @@
 #include "hash_tables.h"
 
 /**
+ * key_checker - function that check if key exist to update
+ * @aux: is the head of the current linked list
+ * @key: is the key
+ * @value: is the value associated with the key
+ *
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+int key_checker(hash_node_t *aux, char *key, char *value)
+{
+	while (aux != NULL)
+	{
+		if (strcmp(aux->key, (char *)key) == 0)
+		{
+			free(aux->value);
+			aux->value = strdup(value);
+			if (aux->value == NULL)
+				return (0);
+			return (1);
+		}
+		aux = aux->next;
+	}
+	return (0);
+}
+/**
  * hash_table_set - function that adds an element to the hash table
  * @ht: is the hash table you want to add or update the key/value to
  * @key: is the key
@@ -11,16 +35,14 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *new_node;
-	hash_node_t *aux;
+	hash_node_t *new_node, *aux;
 
-	if (ht == NULL)
-		return (0);
-	if (key == NULL || strlen(key) == 0)
-		return (0);
-	if (value == NULL)
+	if (ht == NULL || key == NULL || strlen(key) == 0 || value == NULL)
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
+	aux = ht->array[index];
+	if (key_checker(aux, (char *)key, (char *)value) == 1)
+		return (1);
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
@@ -43,11 +65,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		ht->array[index] = new_node;
 		return (1);
 	}
-	else
-	{
-		aux = ht->array[index];
-		new_node->next = aux;
-		ht->array[index] = new_node;
-		return (1);
-	}
+	aux = ht->array[index];
+	new_node->next = aux;
+	ht->array[index] = new_node;
+	return (1);
 }
